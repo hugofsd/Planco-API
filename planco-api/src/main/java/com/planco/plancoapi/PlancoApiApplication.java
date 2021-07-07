233765c77725aba99203ff6504c30e4ba594a9f1
hugofsd <hugo619@outlook.com>
2021-07-07 01:32:02 -0300

    Iniciando desenvolvimento de API

---
+++ planco-api/src/main/java/com/planco/plancoapi/PlancoApiApplication.java
@@ -0,0 +1,13 @@
+package com.planco.plancoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlancoApiApplication.class, args);
+	}
+
+}
---
+++ planco-api/src/test/java/com/planco/plancoapi/PlancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planco.plancoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
