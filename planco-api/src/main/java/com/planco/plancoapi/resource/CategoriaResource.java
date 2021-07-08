3b21aab567960b9cb4c3249eb1e7085a013db0c0
hugofsd <hugo619@outlook.com>
2021-07-07 23:13:40 -0300

    Cadastrando nova categoria com POST,  Get por id de categoria,  Retornar 404 caso não exista a categoria

--- planco-api/src/main/java/com/planco/plancoapi/resource/CategoriaResource.java
+++ planco-api/src/main/java/com/planco/plancoapi/resource/CategoriaResource.java
@@ -1,11 +1,24 @@
 package com.planco.plancoapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.planco.plancoapi.model.Categoria;
 import com.planco.plancoapi.repository.CategoriasRepository;
@@ -20,6 +33,31 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriasRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) //ao terminar, trazer status de criação
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriasRepository.save(categoria);
+		
+		// para indicar o location da categoria cadastrada
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+			response.setHeader("Location", uri.toASCIIString());
+	
+	
+			return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+	
+		  Optional<Categoria> categoria = this.categoriasRepository.findById(codigo);
+		    if (categoria.isPresent())  //isPresent, que nada mais é que uma comparação “obj != null
+				return ResponseEntity.ok(categoria.get());
+			else
+				return ResponseEntity.notFound().build();
+	
 	}
 
 }
