e1cc6859eea3d5d396e08452d4e38d5d27475834
hugofsd <hugo619@outlook.com>
2021-07-12 12:57:31 -0300

    Delete em lançamento

--- planco-api/src/main/java/com/planco/plancoapi/resource/LancamentoResource.java
+++ planco-api/src/main/java/com/planco/plancoapi/resource/LancamentoResource.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,12 @@ public class LancamentoResource {
 		Lancamento lancamentoSalva = lancamentoRepository.save(lancamento);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
 	}
 	
 	
