e51e36de0255392884708d7e278dc19fb6e920de
hugofsd <hugo619@outlook.com>
2021-07-16 01:19:10 -0300

    Movendo o usuário para o banco de dados e gerando senha criptografada

--- planco-api/src/main/java/com/planco/plancoapi/config/SecurityConfig.java
+++ planco-api/src/main/java/com/planco/plancoapi/config/SecurityConfig.java
@@ -1,24 +1,36 @@
 package com.planco.plancoapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity //habilitando segurança
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	//Para este caso, podemos passar entre chaves o ID do Encoder que desejamos utilizar, como a senha não está criptografada, vamos utilizar o {noop}.
     //Caso nossa senha estivesse criptografa com BCrypt (por exemplo), poderíamos utilizar {bcrypt}
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		//user, senha, roles(permissão)
-		 .withUser("admin").password("{noop}admin").roles("ROLE");
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	 // ler criptografia
+	private PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Override
@@ -26,10 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		  .antMatchers("/categorias").permitAll() // qualquer um pode acessar
 		  .anyRequest().authenticated() // o usuario precisa estar validado para fazer qualquer requisição
-		  .and()
-		.httpBasic().and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-		.csrf().disable();
+		  .and().httpBasic().
+		  and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		  .and().csrf().disable();
 
 
 	}
